35dd0ebc721057c1f5ad64a2d5b2f6fe926f8b47
Dan <dngen029@uottawa.ca>
2023-04-05 17:28:00 -0400

    Properly adding google font to all pages atleast I hope so

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,18 +3,26 @@ import NavBar from "./Navbar";
 type Props = {
     children?: JSX.Element | JSX.Element[]
 }
+
+import { Fira_Code } from "next/font/google"
+
+const fira_code = Fira_Code({
+    weight: ["300", "400", "500", "600", "700"],
+    subsets: ["latin"]
+})
+
 const Layout = ({ children }: Props) => {
     const { data: sessionData } = useSession();
     if (sessionData) {
 
         return (
-            <div className="h-screen font-firaCode">
+            <div className={`${fira_code.className} h-screen font-firaCode`}>
                 <NavBar />
                 {children}
             </div>
         )
     }
-    return <>{children}</>
+    return <main className={`${fira_code.className} font-firaCode`}>{children}</main>
 
 }
 
