759b388a9f1de56cdecd7a8292e96c4ef06f1740
Dan <dngen029@uottawa.ca>
2023-04-04 22:19:05 -0400

    Navbar account indicator added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,12 @@ import { Role } from "@prisma/client";
 import { useSession, signOut } from "next-auth/react"
 import Link from "next/link"
 import { useRouter } from "next/router";
+import { useState } from "react";
 const NavBar: React.FC = () => {
     const router = useRouter();
     const currentRoute = router.pathname;
     const { data: sessionData } = useSession();
+    const [openAccountDetails, setToggleAccountDetails] = useState<boolean>(false);
     const linkClass = (path: string) => path === currentRoute ? "text-accent" : "text-gray";
     const navLinks = [
         {
@@ -40,7 +42,7 @@ const NavBar: React.FC = () => {
     return (
         <div className="w-full h-20 bg-bgrd-b flex justify-between items-center p-6">
             <h1>Drive Club</h1>
-            <ul className="flex gap-7">
+            <ul className="flex justify-end gap-7 flex-wrap">
                 {
                     navLinks.map(({ path, adminTitle, title, adminOnly, studentsOnly }, index) => {
                         if (adminOnly) {
@@ -64,10 +66,10 @@ const NavBar: React.FC = () => {
                             }
                         }
                         else {
-                            let useAdminTitle = adminTitle&& sessionData?.user?.role===Role.ADMIN
+                            let useAdminTitle = adminTitle && sessionData?.user?.role === Role.ADMIN
                             return (
                                 <li key={index}>
-                                    <Link href={path} className={linkClass(path)}>{useAdminTitle?adminTitle:title}</Link>
+                                    <Link href={path} className={linkClass(path)}>{useAdminTitle ? adminTitle : title}</Link>
 
                                 </li>
                             )
@@ -75,13 +77,24 @@ const NavBar: React.FC = () => {
                     })
                 }
                 <li>
-                    <button
-                        className="rounded-full bg-white/10 px-10 py-1 font-semibold text-white no-underline transition hover:bg-accent"
-                        onClick={() => signOut()}
-                    >
-                        Sign Out
-                    </button>
-
+                    <span className="hover:text-accent hover:cursor-pointer flex gap-1" onClick={() => setToggleAccountDetails(state => !state)}>
+                        {sessionData?.user.email}
+                        {openAccountDetails?
+<span>&#8593;</span>
+:
+                            <span>&#8595;</span>
+                        }
+                    </span>
+                    {openAccountDetails &&
+                        <div className="fixed w-[250px] h-fit p-3 bg-bgrd-b flex justify-center">
+                            <button
+                                className="rounded-full bg-white/10 px-10 py-1 font-semibold text-white no-underline transition hover:bg-accent"
+                                onClick={() => signOut()}
+                            >
+                                Sign Out
+                            </button>
+                        </div>
+                    }
                 </li>
 
             </ul>
